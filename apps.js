4a0e9b19b57a14e992c98f93b14de6d97e48096b
Vince Reyes <vincentejosereyes@gmail.com>
2018-04-07 21:34:47 -0700

    initial commit

---
+++ apps.js
@@ -0,0 +1,69 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/cats') {
+        fs.readFile('cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/images/gtr.jpg'){
+    // notice we won't include the utf8 encoding
+    fs.readFile('./images/gtr.jpg', function(errors, contents){
+        response.writeHead(200, {'Content-type': 'image/jpg'});
+        response.write(contents);
+        response.end();
+    })
+}    else if(request.url === '/images/gtr2.jpg'){
+    // notice we won't include the utf8 encoding
+    fs.readFile('./images/gtr2.jpg', function(errors, contents){
+        response.writeHead(200, {'Content-type': 'image/jpg'});
+        response.write(contents);
+        response.end();
+    })
+}
+    else if(request.url === '/images/engine.jpg'){
+    // notice we won't include the utf8 encoding
+    fs.readFile('./images/engine.jpg', function(errors, contents){
+        response.writeHead(200, {'Content-type': 'image/jpg'});
+        response.write(contents);
+        response.end();
+    })
+  }
+    else if(request.url === '/stylesheets/style.css'){
+    fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+     response.writeHead(200, {'Content-type': 'text/css'});
+     response.write(contents);
+     response.end();
+    })
+  }
+    // request didn't match anything:
+    else {
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 7077");
